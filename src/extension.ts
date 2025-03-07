60b11ab2b37335896594aea78cc01a43cb6e889e
tmk3t <tmkt20668@gmail.com>
2025-03-07 13:40:39 +0900

    国際化対応: VSCode拡張機能の多言語化を実装
    - パッケージ設定を国際化対応に更新
    - 日本語と英語のローカライゼーションファイルを追加
    - `@vscode/l10n`パッケージを導入
    - 拡張機能のアクティベーションメッセージを多言語化
    - パッケージ設定の説明文を言語リソースに移行

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,8 @@ import { copyDiagnosticsHandler } from './commands/errorCommands';
 import { copySelectedVitestResultsHandler } from './commands/vitestCommands';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Extension "easy-copy-errors" is now active');
+
+  console.log(vscode.l10n.t('extension activated'));
 
   // 診断情報コピーコマンドを1つに統合
   const diagnosticsCommand = vscode.commands.registerCommand(
